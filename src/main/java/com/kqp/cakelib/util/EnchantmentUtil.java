1c93afab51e09cd755c7dc6359ec69047fce2e50
cakewhip <phan.kavin@gmail.com>
2020-12-25 12:25:14 -0500

    Fix mixins and add Enchantment util

---
+++ src/main/java/com/kqp/cakelib/util/EnchantmentUtil.java
@@ -0,0 +1,27 @@
+package com.kqp.cakelib.util;
+
+import java.util.Optional;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.ItemStack;
+
+public class EnchantmentUtil {
+
+    private static int getLevel(
+        Optional<Enchantment> optionalEnchantment,
+        ItemStack itemStack
+    ) {
+        return optionalEnchantment
+            .map(
+                enchantment ->
+                    EnchantmentHelper.getLevel(enchantment, itemStack)
+            )
+            .orElse(0);
+    }
+
+    private static int getMaxLevel(Optional<Enchantment> optionalEnchantment) {
+        return optionalEnchantment
+            .map(enchantment -> enchantment.getMaxLevel())
+            .orElse(0);
+    }
+}
